1fddb3e5ba6205dff75df98b3d1cd957fe75272f
Moritz <mosum@google.com>
2024-06-13 15:12:38 +0200

    Add some options

--- lib/dart_intl4x_example.dart
+++ lib/dart_intl4x_example.dart
@@ -1,5 +1,15 @@
 import 'package:intl4x/intl4x.dart';
+import 'package:intl4x/number_format.dart';
 
 String calculate(num i, Locale locale) {
-  return Intl(locale: locale).numberFormat().format(i);
+  return Intl(locale: locale)
+      .numberFormat(
+        NumberFormatOptions.custom(
+          digits: Digits.withFractionDigits(
+            maximum: 6,
+            minimum: 5,
+          ),
+        ),
+      )
+      .format(i);
 }
